d7e6f8f51af4b1523ed2ffe0357a3e25fa944903
NetWin <m.seiffert@snapaddy.com>
2022-08-17 15:28:20 +0200

    chore: remove cli functionality and (then) unnecessary dependency

--- webpack.config.js
+++ webpack.config.js
@@ -28,35 +28,7 @@ function interpreterConfig(
   };
 }
 
-function cliConfig(
-  mode = 'development',
-  filename = 'js-interpreter.js',
-  devtool = false
-) {
-  return {
-    mode,
-    entry: path.resolve(__dirname, 'src', 'cli.js'),
-    target: 'node',
-    output: {
-      filename,
-      path: path.resolve(__dirname, 'lib'),
-    },
-    plugins: [
-      new webpack.ProvidePlugin({
-        acorn: path.resolve(__dirname, 'original-repo', 'acorn.js')
-      }),
-      new webpack.BannerPlugin({
-        banner: '#!/usr/bin/env node',
-        raw: true,
-      }),
-    ],
-    devtool: devtool ? 'cheap-source-map' : false,
-  };
-}
-
 module.exports = [
   interpreterConfig('development', 'js-interpreter.js', false),
-  interpreterConfig('production', 'js-interpreter.min.js', true),
-  cliConfig('development', 'cli.js', false),
-  cliConfig('production', 'cli.min.js', true),
+  interpreterConfig('production', 'js-interpreter.min.js', true)
 ];
