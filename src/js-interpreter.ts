9a721f9020f2611443367a000862385b8b78da38
Markus Seiffert <m.seiffert@snapaddy.com>
2024-06-25 15:06:04 +0200

    fix(*): correctly assign nativeGlobal

--- src/js-interpreter.ts
+++ src/js-interpreter.ts
@@ -1,9 +1,17 @@
 import { JSInterpreter } from './types';
 
+declare global {
+  var acorn: any;
+  var Interpreter: JSInterpreter;
+  interface Window {
+    acorn: any;
+  }
+}
+
 // Load acorn and assign it to the global scope
 const acorn = require('../original-repo/acorn');
 
-const nativeGlobal = global as any;
+const nativeGlobal = (typeof globalThis === 'undefined') ? this || window : globalThis;
 nativeGlobal.acorn = acorn;
 
 // Load the js-interpreter
